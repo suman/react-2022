db219a1022374aba845dda7acd6063c1f7890e94
suman <sumanbogati@gmail.com>
2022-11-22 18:03:41 +0530

    Redux by older style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Dashboard from './Components/MainSection/Dashboard';
+import Reducers from './reducers';
+import thunk from 'redux-thunk';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(Reducers, applyMiddleware(thunk));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={store}>
+        <Dashboard />
+      </Provider>
+
     </div>
   );
 }
